f7df7706081cb1691a1a445880a65a63865e1872
rishirajkumar97 <jrishi007@gmail.com>
2025-03-10 03:38:52 +0530

    Update News Service Scheduler Timing

--- news_stock_service/scheduler/jobs.py
+++ news_stock_service/scheduler/jobs.py
@@ -3,6 +3,6 @@ from services.news_service import fetch_and_store_news
 from services.stock_service import save_ftse_price
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(fetch_and_store_news, "interval", minutes=1)
+scheduler.add_job(fetch_and_store_news, "interval", minutes=30)
 scheduler.add_job(save_ftse_price, "interval", minutes=1)
 scheduler.start()
